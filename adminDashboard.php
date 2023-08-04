70688cfc19b95841b6d898509069faf37463b94f
0okitoaster <s2020579@usls.edu.ph>
2023-08-04 12:43:21 +0800

    Update adminDashboard.php

--- adminDashboard.php
+++ adminDashboard.php
@@ -62,15 +62,15 @@ if(!empty($_SESSION["user_id"])){
                             <div class="card col-md-4 h100">
                                 <div class="cardHeader text-white bg-lasalle-grn">Canceled Bookings</div>
                                 <a class="cardFooter bg-lasalle-grnAlt" href="userBookings.php">
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">➤</span>
+                                    <span class="float-left text-white">View Details</span>
+                                    <span class="float-right text-white">➤</span>
                                 </a>
                             </div>
                             <div class="card col-md-4 h100">
                                 <div class="cardHeader text-white bg-lasalle-grn">Trip Schedule</div>
                                 <a class="cardFooter bg-lasalle-grnAlt" href="userReservations.php">
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">➤</span>
+                                    <span class="float-left text-white">View Details</span>
+                                    <span class="float-right text-white">➤</span>
                                 </a>
                             </div>
                     </div>
